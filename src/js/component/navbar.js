edab7bf8e81478e745387fef2ff90bd9f702590b
Taylor Hart <thart003@ucr.edu>
2021-12-07 23:07:41 +0000

    navbar alteration

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,28 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Star Wars Bloglist</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+			<div className="ml-auto d-inline-block">
+				<Link to="/people">
+					<span className="navbar-brand mb-0 h1">People</span>
+				</Link>
+				<Link to="/planets">
+					<span className="navbar-brand mb-0 h1">Planets</span>
+				</Link>
+				<Link to="/vehicles">
+					<span className="navbar-brand mb-0 h1">Vehicles</span>
+				</Link>
+				<Link to="/store/favorites">
+					<span className="navbar-brand mb-0 h1">
+						<i
+							className="fas fa-star bg-light text-dark rounded p-2"
+							style={{
+								fontSize: "1.3rem",
+								cursor: "pointer"
+							}}
+						/>
+					</span>
 				</Link>
 			</div>
 		</nav>
