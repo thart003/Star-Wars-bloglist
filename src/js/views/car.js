4094fcdeaf6ce49ce73a3cbc1a3d9826c17aee88
Taylor Hart <thart003@ucr.edu>
2022-01-02 01:39:34 +0000

    amend car.js

--- src/js/views/car.js
+++ src/js/views/car.js
@@ -4,24 +4,32 @@ import { useParams } from "react-router-dom";
 import { starWarsApiFetch } from "../views/people";
 import useSWR from "swr";
 
+import SandCrawler from "../../img/sandy.png";
+import Landspeeder from "../../img/landspeeder.jpeg";
+import SkyHopper from "../../img/skyhopper.png";
+import TieFighter from "../../img/tie-fighter.jpeg";
+import Snowspeeder from "../../img/snowspeeder.jpg";
+import AtAt from "../../img/atat.jpeg";
+import TieBomber from "../../img/tieBomber.jpeg";
+import AtSt from "../../img/atst.jpg";
+import CloudCar from "../../img/stormCloudCar.png";
+import SailBarge from "../../img/SailBarge.jpeg";
+
 export const Car = () => {
 	const params = useParams();
 	const { data } = useSWR("/api/vehicles/" + params.id, starWarsApiFetch);
 
 	const images = {
-		"1": "https://d2eehagpk5cl65.cloudfront.net/img/c800x450-w800-q80/uploads/2016/04/14608132418959.jpg",
-		"2":
-			"https://static.wikia.nocookie.net/starwars/images/5/54/X34-landspeeder.jpg/revision/latest?cb=20080316031428",
-		"3": "https://dnqe9n02rny0n.cloudfront.net/sets/images/11099/75081_alt_2_md.jpg",
-		"4": "https://bbts1.azureedge.net/images/p/full/2021/01/0cbfc29f-020e-479d-98f4-18252af8a8fd.jpg",
-		"5":
-			"https://static.wikia.nocookie.net/starwars/images/4/44/T-47_Speeder_DICE.png/revision/latest?cb=20151106063152",
-		"6": "https://www.denofgeek.com/wp-content/uploads/2016/05/atat-walkers.jpg?fit=620%2C349",
-		"7": "https://static.wikia.nocookie.net/starwars/images/9/9b/TIE_Bomber.jpg/revision/latest?cb=20081012232104",
-		"8":
-			"https://static.wikia.nocookie.net/starwars/images/f/ff/ATST-SWBdice.png/revision/latest/top-crop/width/360/height/360?cb=20151110032640",
-		"9": "https://m.media-amazon.com/images/I/71xhRX2PQ3L._AC_SX425_.jpg",
-		"10": "https://static.wikia.nocookie.net/starwars/images/a/a5/Eclipsesb.jpg/revision/latest?cb=20060406082922"
+		"1": SandCrawler,
+		"2": Landspeeder,
+		"3": SkyHopper,
+		"4": TieFighter,
+		"5": Snowspeeder,
+		"6": AtAt,
+		"7": TieBomber,
+		"8": AtSt,
+		"9": CloudCar,
+		"10": SailBarge
 	};
 
 	return (
